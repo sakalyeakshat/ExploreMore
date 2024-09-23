7f0d0ec76f5d4788f979363af2d0480fdc1fa5fc
unknown <mayanktilwankar2355@gmail.com>
2024-09-24 00:00:23 +0530

    as

--- pages/Components/HomeNavbar.jsx
+++ pages/Components/HomeNavbar.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -10,18 +24,16 @@ const Navbar = () => {
   return (
     <nav className="bg-white text-black">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/images/logo.png" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          {/* <img src="/images/logo.png" className="h-8 sm:h-10" alt="Logo" /> */}
+          <span className="text-xl sm:text-2xl font-semibold whitespace-nowrap">
             Vakratunda Homestay
           </span>
         </a>
         <button
           onClick={toggleMenu}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          className="inline-flex items-center w-auto  h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}>
           <span className="sr-only">Toggle menu</span>
@@ -42,7 +54,7 @@ const Navbar = () => {
         </button>
         <div
           className={`${
-            isMenuOpen ? "block" : "hidden"
+            isMenuOpen || !isMobile ? "block" : "hidden"
           } w-full md:block md:w-auto`}
           id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
