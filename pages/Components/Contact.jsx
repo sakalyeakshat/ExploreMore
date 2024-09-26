2e59da7aca9214416e43c59af4ee62a3c7cd601e
unknown <mayanktilwankar2355@gmail.com>
2024-09-26 23:19:27 +0530

    okii

--- pages/Components/Contact.jsx
+++ pages/Components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
     setStatus("Submitting...");
 
     try {
-      const res = await fetch("/api/postEnquiry", {
+      const res = await fetch("/api/form-data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
